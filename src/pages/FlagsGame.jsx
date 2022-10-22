5f2388013fc6940212f34f0ce5a476bcca55c218
Lucio Vasquez <castrocociolucio@gmail.com>
2022-10-22 18:14:50 -0300

    :lipstick: Adding styles to the StartGame button in FlagsGame

--- src/pages/FlagsGame.jsx
+++ src/pages/FlagsGame.jsx
@@ -31,6 +31,7 @@ const FlagsGame = () => {
 
       {gameNotStarted && (
         <button
+          className='bg-main-yellow text-white rounded-xl h-10 flex items-center w-64 justify-center mx-auto mb-4 shadow-main-shadow transition ease-in-out delay-75 glow'
           onClick={() => {
             setGameStart(false);
             createNewPattern();
