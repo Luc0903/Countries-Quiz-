f43aaf43fd628ccd339c1504480a97217409b898
Lucio Vasquez <castrocociolucio@gmail.com>
2022-10-22 18:11:03 -0300

    :poop: Correcting small mistake in FlagsGame Component

--- src/pages/FlagsGame.jsx
+++ src/pages/FlagsGame.jsx
@@ -12,32 +12,33 @@ const FlagsGame = () => {
 
   return (
     <div className=' h-screen w-screen flex flex-col justify-center items-center'>
-      <div className=' bg-white w-full max-w-md rounded-3xl  shadow-main-shadow'>
-        {!gameNotStarted && <p>{correctAnswer.name.common}</p>}
-        <div>
-          {gameUIPattern.map((choice) => {
-            return <AnswersButton key={uuid()} text={choice.name.common} correctAnswer={correctAnswer} />;
-          })}
-        </div>
-        {!gameNotStarted && (
+      {!gameNotStarted && (
+        <div className=' bg-white w-full max-w-md rounded-3xl  shadow-main-shadow'>
+          <p>{correctAnswer.name.common}</p>
+          <div>
+            {gameUIPattern.map((choice) => {
+              return <AnswersButton key={uuid()} text={choice.name.common} correctAnswer={correctAnswer} />;
+            })}
+          </div>
           <button
             onClick={reset}
             className=' normalGlow w-20 h-10 text-white font-normal bg-main-yellow rounded-lg mb-4'
           >
             Next
           </button>
-        )}
-        {gameNotStarted && (
-          <button
-            onClick={() => {
-              setGameStart(false);
-              createNewPattern();
-            }}
-          >
-            Start Game
-          </button>
-        )}
-      </div>
+        </div>
+      )}
+
+      {gameNotStarted && (
+        <button
+          onClick={() => {
+            setGameStart(false);
+            createNewPattern();
+          }}
+        >
+          Start Game
+        </button>
+      )}
     </div>
   );
 };
