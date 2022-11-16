9727125eefa58dcdb0264a0a0b05438ed224b13b
Lucio Vasquez <castrocociolucio@gmail.com>
2022-11-16 20:20:56 -0300

    :boom: Introducing a new idea to useSortCountries

--- src/hooks/useSortCountries.js
+++ src/hooks/useSortCountries.js
@@ -1,37 +1,67 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const useSortCountries = (countriesInfoArray) => {
-  const [gameUIPattern, setUIGamePattern] = useState([]);
-  const [gameNotStarted, setGameStart] = useState(true);
-  const [correctAnswer, setCorrectAnswer] = useState({
-    name: {
-      common: 'flag will be here',
-    },
-    capital: [],
-    flags: {},
-  });
-  let gamePattern = [];
+  const [gamePattern, setGamePattern] = useState([]);
+  const [correctAnswer, setCorrectAnswer] = useState({});
+
+  let noRepeatedCountriesPattern = [];
   let temporaryPattern = [];
+
   const getRandomNumber = () => Math.floor(Math.random() * 250);
   const getRandomCountry = () => countriesInfoArray[getRandomNumber()];
 
   function createNewPattern() {
-    while (gamePattern.length < 4) {
+    while (noRepeatedCountriesPattern.length < 4) {
       let possibleCountry = getRandomCountry();
       temporaryPattern.push(possibleCountry);
-      gamePattern = [...new Set(temporaryPattern)];
+      noRepeatedCountriesPattern = [...new Set(temporaryPattern)];
     }
-    setUIGamePattern(gamePattern);
-    setCorrectAnswer(gamePattern[Math.floor(Math.random() * 4)]);
+    setGamePattern(noRepeatedCountriesPattern);
+    setCorrectAnswer(noRepeatedCountriesPattern[Math.floor(Math.random() * 4)]);
   }
 
-  function reset() {
-    gamePattern = [];
+  useEffect(() => {
+    createNewPattern();
+  }, []);
+
+  function Reset() {
+    noRepeatedCountriesPattern = [];
     temporaryPattern = [];
     createNewPattern();
   }
 
-  return { gameUIPattern, gameNotStarted, setGameStart, reset, createNewPattern, correctAnswer };
+  return { gamePattern, correctAnswer, Reset };
+  // const [gameUIPattern, setUIGamePattern] = useState([]);
+  // const [gameNotStarted, setGameStart] = useState(true);
+  // const [correctAnswer, setCorrectAnswer] = useState({
+  //   name: {
+  //     common: 'flag will be here',
+  //   },
+  //   capital: [],
+  //   flags: {},
+  // });
+  // let gamePattern = [];
+  // let temporaryPattern = [];
+  // const getRandomNumber = () => Math.floor(Math.random() * 250);
+  // const getRandomCountry = () => countriesInfoArray[getRandomNumber()];
+
+  // function createNewPattern() {
+  //   while (gamePattern.length < 4) {
+  //     let possibleCountry = getRandomCountry();
+  //     temporaryPattern.push(possibleCountry);
+  //     gamePattern = [...new Set(temporaryPattern)];
+  //   }
+  //   setUIGamePattern(gamePattern);
+  //   setCorrectAnswer(gamePattern[Math.floor(Math.random() * 4)]);
+  // }
+
+  // function reset() {
+  //   gamePattern = [];
+  //   temporaryPattern = [];
+  //   createNewPattern();
+  // }
+
+  // return { gameUIPattern, gameNotStarted, setGameStart, reset, createNewPattern, correctAnswer };
 };
 
 export default useSortCountries;
