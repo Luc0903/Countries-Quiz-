ca00d1616ef94742ef71b9dc3387b2deb8d3ffa6
Lucio Vasquez <castrocociolucio@gmail.com>
2022-11-16 22:44:37 -0300

    :recycle: Making a big change to solve a bug at the begging of the games

--- src/hooks/useSortCountries.js
+++ src/hooks/useSortCountries.js
@@ -2,7 +2,13 @@ import { useState, useRef, useEffect } from 'react';
 
 const useSortCountries = (countriesInfoArray) => {
   const [gamePattern, setGamePattern] = useState([]);
-  const [correctAnswer, setCorrectAnswer] = useState({});
+  const [correctAnswer, setCorrectAnswer] = useState({
+    name: {
+      common: 'flag will be here',
+    },
+    capital: [],
+    flags: {},
+  });
 
   let noRepeatedCountriesPattern = [];
   let temporaryPattern = [];
@@ -31,37 +37,6 @@ const useSortCountries = (countriesInfoArray) => {
   }
 
   return { gamePattern, correctAnswer, Reset };
-  // const [gameUIPattern, setUIGamePattern] = useState([]);
-  // const [gameNotStarted, setGameStart] = useState(true);
-  // const [correctAnswer, setCorrectAnswer] = useState({
-  //   name: {
-  //     common: 'flag will be here',
-  //   },
-  //   capital: [],
-  //   flags: {},
-  // });
-  // let gamePattern = [];
-  // let temporaryPattern = [];
-  // const getRandomNumber = () => Math.floor(Math.random() * 250);
-  // const getRandomCountry = () => countriesInfoArray[getRandomNumber()];
-
-  // function createNewPattern() {
-  //   while (gamePattern.length < 4) {
-  //     let possibleCountry = getRandomCountry();
-  //     temporaryPattern.push(possibleCountry);
-  //     gamePattern = [...new Set(temporaryPattern)];
-  //   }
-  //   setUIGamePattern(gamePattern);
-  //   setCorrectAnswer(gamePattern[Math.floor(Math.random() * 4)]);
-  // }
-
-  // function reset() {
-  //   gamePattern = [];
-  //   temporaryPattern = [];
-  //   createNewPattern();
-  // }
-
-  // return { gameUIPattern, gameNotStarted, setGameStart, reset, createNewPattern, correctAnswer };
 };
 
 export default useSortCountries;
