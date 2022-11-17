d29b9396d079c9581011087c9e21d05e293dd77b
Lucio Vasquez <castrocociolucio@gmail.com>
2022-11-16 22:49:57 -0300

    :boom: Implementing the new logic to the provinces game component

--- src/pages/ProvincesGame.jsx
+++ src/pages/ProvincesGame.jsx
@@ -10,13 +10,13 @@ import StartGameButton from '../components/StartGameButton';
 
 const ProvincesGame = () => {
   const { allCountriesData } = useContext(CountriesData);
-  const { gameUIPattern, gameNotStarted, setGameStart, reset, createNewPattern, correctAnswer } =
-    useSortCountries(allCountriesData);
+
+  const { gamePattern, correctAnswer, Reset } = useSortCountries(allCountriesData);
 
   return (
     <div className=' h-screen w-screen flex flex-col justify-center items-center'>
       <p className=' text-quiz-title font-bold text-2xl countryQuizAlignment'>COUNTRY QUIZ</p>
-      {!gameNotStarted && (
+      {gamePattern && (
         <div className=' bg-white w-full max-w-md rounded-3xl shadow-main-shadow relative'>
           <div className=' absolute w-36 h-36 -top-14 right-7'>
             <img src={MainImage} alt='A man standing next to a globe' />
@@ -25,19 +25,18 @@ const ProvincesGame = () => {
             {correctAnswer.capital[0]} is the capital of
           </p>
           <div>
-            {gameUIPattern.map((choice) => {
+            {gamePattern.map((choice) => {
               return <AnswersButton key={uuid()} text={choice.name.common} correctAnswer={correctAnswer} />;
             })}
           </div>
           <button
-            onClick={reset}
+            onClick={Reset}
             className=' NEXTbuttonAlignment w-20 h-10 text-white font-normal bg-main-yellow rounded-lg mb-4 glow'
           >
             Next
           </button>
         </div>
       )}
-      {gameNotStarted && <StartGameButton setGameStart={setGameStart} createNewPattern={createNewPattern} />}
     </div>
   );
 };
